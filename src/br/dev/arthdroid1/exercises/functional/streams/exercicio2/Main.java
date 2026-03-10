605a093f2c25b869d896d2825367ede5d0c12095
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-10 15:32:36 -0300

    Finish exercise 2 streams

--- src/br/dev/arthdroid1/exercises/functional/streams/exercicio2/Main.java
+++ src/br/dev/arthdroid1/exercises/functional/streams/exercicio2/Main.java
@@ -1,9 +1,53 @@
 package br.dev.arthdroid1.exercises.functional.streams.exercicio2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+
+import br.dev.arthdroid1.exercises.functional.streams.exercicio2.entities.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		List<Product> products = new ArrayList<>();
+		
+		System.out.println("Enter the file path: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			while(line !=null) {
+				String[] fields = line.split(",");
+				products.add(new Product(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+			
+		}
+		catch(IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+		}
+		
+		System.out.println("Input a category: ");
+		String category = sc.nextLine();
+		
+		List<Product> filteredProducts = products.stream().filter(p -> p.getCategory().equalsIgnoreCase(category))
+				.sorted(Comparator.comparingDouble(Product::getPrice).reversed()).toList();
+		
+		double averagePrice = products.stream()
+			    .mapToDouble(Product::getPrice)
+			    .average()
+			    .orElse(0.0);
+		
+		filteredProducts.forEach(System.out::println);
+		System.out.printf("Price average: %.2f", averagePrice);
+		
+		sc.close();
 
 	}
 
---
+++ src/br/dev/arthdroid1/exercises/functional/streams/exercicio2/entities/Product.java
@@ -0,0 +1,46 @@
+package br.dev.arthdroid1.exercises.functional.streams.exercicio2.entities;
+
+public class Product {
+	private String name;
+	private String category;
+	private Double price;
+	
+	public Product(String name, String category, Double price) {
+		this.name = name;
+		this.category = category;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", category=" + category + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+}
